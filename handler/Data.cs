f0cea73176b0fd03e5af4fc20f194fcc69bbcb64
romance <66000596+romance999@users.noreply.github.com>
2022-09-28 16:25:51 -0400

    Create Data.cs

---
+++ handler/Data.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ezafk.handler
+{
+    internal class Data
+    {
+        public static string valProcess = "VALORANT-Win64-Shipping";
+
+        public static bool antiafk;
+    }
+}
