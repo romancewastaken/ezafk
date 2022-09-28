46241dc59f3a1b12f3034e475c4f3e74d4b7c8fc
romance <66000596+romance999@users.noreply.github.com>
2022-09-28 16:26:16 -0400

    Add files via upload

---
+++ handler/nativeMethods.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ezafk.handler
+{
+    internal class nativeMethods
+    {
+        public struct MARGINS
+        {
+            public int leftWidth;
+            public int rightWidth;
+            public int topHeight;
+            public int bottomHeight;
+        }
+
+        /// <summary>
+        /// Managed Wrapper Class for WinAPI Calls. Why? See <see href="https://docs.microsoft.com/en-us/previous-versions/visualstudio/visual-studio-2012/ms182161(v=vs.110)?redirectedfrom=MSDN">CA1060</see> for more info.
+        /// </summary>
+        internal class NativeMethods
+        {
+            /// <summary>
+            /// Sends the specified message to a window or windows. See <see href="https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-sendmessage">SendMessage function (winuser.h)</see> for more info.
+            /// </summary>
+            /// <param name="hWnd">A handle to the window whose procedure will receive the message.</param>
+            /// <param name="Msg">The message to be sent.  See <see href="https://docs.microsoft.com/en-us/windows/win32/winmsg/about-messages-and-message-queues">System Defined Messages</see> for more info.</param>
+            /// <param name="wParam">Additional message specific information.</param>
+            /// <param name="lParam">Additional message specific information.</param>
+            /// <returns>Value of <see cref="int"/> (Win32 LRESULT) specifies the result of the message processing; it depends on the message sent. Use Marshal.GetLastWin32Error() for error details.</returns>
+            [DllImport("user32.dll", SetLastError = true)]
+            public static extern int SendMessage(IntPtr hWnd, uint Msg, int wParam, int lParam);
+
+            /// <summary>
+            /// Releases the mouse capture from a window in the current thread and restores normal mouse input processing See <see href="https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-releasecapture">ReleaseCapture function (winuser.h)</see> for more info.
+            /// </summary>
+            /// <returns>If the function succeeds, the return value is nonzero. Use Marshal.GetLastWin32Error() for error details.</returns>
+            [DllImport("user32.dll", SetLastError = true)]
+            public static extern bool ReleaseCapture();
+
+            /// <summary>
+            /// Sets the value of Desktop Window Manager (DWM) non-client rendering attributes for a window. See <see href="https://docs.microsoft.com/en-us/windows/win32/api/dwmapi/nf-dwmapi-dwmsetwindowattribute">DwmSetWindowAttribute function (dwmapi.h)</see> for more info.
+            /// </summary>
+            /// <param name="hwnd">The handle to the window for which the attribute value is to be set.</param>
+            /// <param name="dwAttribute">A flag describing which value to set, specified as a value of the <see href="https://docs.microsoft.com/en-us/windows/win32/api/dwmapi/ne-dwmapi-dwmwindowattribute">DWMWINDOWATTRIBUTE</see> enumeration. This parameter specifies which attribute to set, and the pvAttribute parameter points to an object containing the attribute value.</param>
+            /// <param name="pvAttribute">A pointer to an object containing the attribute value to set. The type of the value set depends on the value of the dwAttribute parameter. </param>
+            /// <param name="cbAttribute">The size, in bytes, of the attribute value being set via the pvAttribute parameter. The type of the value set, and therefore its size in bytes, depends on the value of the dwAttribute parameter.</param>
+            /// <returns>If the function succeeds, it returns S_OK. Otherwise, it returns an <see href="https://docs.microsoft.com/en-us/windows/win32/com/structure-of-com-error-codes">HRESULT</see> error code. Use Marshal.GetLastWin32Error() for error details.</returns>
+            [DllImport("dwmapi.dll", SetLastError = true)]
+            public static extern int DwmSetWindowAttribute(IntPtr hwnd, uint dwAttribute, ref int pvAttribute, int cbAttribute);
+
+            /// <summary>
+            /// Extends the window frame into the client area. See <see href="https://docs.microsoft.com/en-us/windows/win32/api/dwmapi/nf-dwmapi-dwmextendframeintoclientarea">DwmExtendFrameIntoClientArea function (dwmapi.h)</see> for more info.
+            /// </summary>
+            /// <param name="hWnd">The handle to the window in which the frame will be extended into the client area.</param>
+            /// <param name="pMarInset">A pointer to a <see cref="MARGINS"/> structure that describes the margins to use when extending the frame into the client area.</param>
+            /// <returns>If this function succeeds, it returns S_OK. Otherwise, it returns an HRESULT error code. Use Marshal.GetLastWin32Error() for error details.</returns>
+            [DllImport("dwmapi.dll", SetLastError = true)]
+            public static extern int DwmExtendFrameIntoClientArea(IntPtr hWnd, ref MARGINS pMarInset);
+
+            /// <summary>
+            /// Obtains a value that indicates whether Desktop Window Manager (DWM) composition is enabled. See <see href="https://docs.microsoft.com/en-us/windows/win32/api/dwmapi/nf-dwmapi-dwmiscompositionenabled">DwmIsCompositionEnabled function (dwmapi.h)</see> for more info.
+            /// </summary>
+            /// <param name="pfEnabled">A pointer to a value that, when this function returns successfully, receives TRUE if DWM composition is enabled; otherwise, FALSE.</param>
+            /// <returns>If this function succeeds, it returns S_OK. Otherwise, it returns an HRESULT error code. Use Marshal.GetLastWin32Error() for error details.</returns>
+            [DllImport("dwmapi.dll", SetLastError = true)]
+            public static extern int DwmIsCompositionEnabled(ref int pfEnabled);
+        }
+
+        /// <summary>
+        /// Win32 API Constant Values.
+        /// </summary>
+        internal class Constants
+        {
+            /// <summary>
+            /// The <see href="https://docs.microsoft.com/en-us/windows/win32/gdi/wm-ncpaint">WM_NCPAINT message</see> is sent to a window when its frame must be painted.
+            /// </summary>
+            public const uint WM_NCPAINT = 0x0085;
+            /// <summary>
+            /// Posted when the user presses the left mouse button while the cursor is within the nonclient area of a window. This message is posted to the window that contains the cursor. If a window has captured the mouse, this message is not posted. See <see href="https://docs.microsoft.com/en-us/windows/win32/inputdev/wm-nclbuttondown">WM_NCLBUTTONDOWN message</see> for more info.
+            /// </summary>
+            public const uint WM_NCLBUTTONDOWN = 0x00A1;
+            /// <summary>
+            /// Sent to a window in order to determine what part of the window corresponds to a particular screen coordinate.
+            /// </summary>
+            public const uint WM_NCHITTEST = 0x0084;
+            /// <summary>
+            /// Indicates the cursor is in a title bar. Part of WM_NCHITTEST message. See <see href="https://docs.microsoft.com/en-us/windows/win32/inputdev/wm-nchittest">WM_NCHITTEST message</see> for more info.
+            /// </summary>
+            public const uint HT_CAPTION = 0x2;
+
+            /// <summary>
+            /// Undocumented DWM API | Enables Dark Titlebar on Win10
+            /// </summary>
+            public const uint DWMA_USE_IMMERSIVE_DARK_MODE = 0x00000014;
+            /// <summary>
+            /// Undocumented DWM API | Enables Dark Titlebar on Win10 (before 20H1)
+            /// </summary>
+            public const uint DWMA_USE_IMMERSIVE_DARK_MODE_OLD = 0x00000013;
+
+            /// <summary>
+            /// Window Class Value for CS_DROPSHADOW basic effect.
+            /// </summary>
+            public const uint CS_DROPSHADOW = 0x00020000;
+        }
+    }
+}
---
+++ handler/windowEx.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Drawing;
+using static ezafk.handler.nativeMethods;
+using System.Windows.Forms;
+
+namespace ezafk.handler
+{
+    internal class windowEx
+    {
+        private readonly Form _formObject = null;
+
+        public List<Tuple<Control, int, int>> _shadowControls = new List<Tuple<Control, int, int>>();
+        private Bitmap _shadowBmp = null;
+
+        public windowEx(Form target, bool doDrag = true)
+        {
+            _formObject = target;
+
+            if (doDrag)
+                _formObject.MouseDown += FormObject_MouseDown;
+        }
+
+        private void DrawShadowSmooth(GraphicsPath gp, int intensity, int radius, Bitmap dest)
+        {
+            if (_formObject.Handle == null)
+                return;
+
+            using (Graphics g = Graphics.FromImage(dest))
+            {
+                g.Clear(Color.Transparent);
+                g.CompositingMode = CompositingMode.SourceCopy;
+                double alpha = 0;
+                double astep = 0;
+                double astepstep = (double)intensity / radius / (radius / 2D);
+                for (int thickness = radius; thickness > 0; thickness--)
+                {
+                    using (Pen p = new Pen(Color.FromArgb((int)alpha, 0, 0, 0), thickness))
+                    {
+                        p.LineJoin = LineJoin.Round;
+                        g.DrawPath(p, gp);
+                    }
+                    alpha += astep;
+                    astep += astepstep;
+                }
+            }
+        }
+        public void Hook_Paint(object sender, PaintEventArgs e)
+        {
+            if (sender == null)
+                return;
+            if ((sender as Control).Handle == null)
+                return;
+
+            if (_shadowBmp == null || _shadowBmp.Size != _formObject.Size)
+            {
+                _shadowBmp?.Dispose();
+                _shadowBmp = new Bitmap(_formObject.Width, _formObject.Height, PixelFormat.Format32bppArgb);
+            }
+            foreach (Tuple<Control, int, int> control in _shadowControls)
+            {
+                if (!control.Item1.Visible || !control.Item1.TopLevelControl.Visible)
+                    continue;
+
+                using (GraphicsPath gp = new GraphicsPath())
+                {
+                    gp.AddRectangle(new Rectangle(control.Item1.Location.X, control.Item1.Location.Y, control.Item1.Size.Width, control.Item1.Size.Height));
+                    DrawShadowSmooth(gp, control.Item2, control.Item3, _shadowBmp);
+                    /// item2 usually 90, item3 usually 35
+                }
+                e.Graphics.DrawImage(_shadowBmp, new Point(0, 0));
+            }
+        }
+
+        public void TryEnableDarkTheme()
+        {
+            int value = 1;
+
+            if (NativeMethods.DwmSetWindowAttribute(_formObject.Handle, Constants.DWMA_USE_IMMERSIVE_DARK_MODE, ref value, 4) != 0)
+            {
+                NativeMethods.DwmSetWindowAttribute(_formObject.Handle, Constants.DWMA_USE_IMMERSIVE_DARK_MODE_OLD, ref value, 4);
+            }
+        }
+
+        #region Window Movement
+
+        public void FormObject_MouseDown(object source, MouseEventArgs ev)
+        {
+            if (ev.Button == MouseButtons.Left)
+            {
+                NativeMethods.ReleaseCapture();
+
+                Type type = source.GetType().BaseType;
+
+                if (type.FullName is "System.Windows.Forms.Form")
+                {
+                    NativeMethods.SendMessage(((Form)source).Handle, 0xA1, 0x2, 0);
+                    return;
+                }
+
+                try
+                {
+                    Control ctrl = (Control)source;
+
+                    NativeMethods.SendMessage(ctrl.FindForm().Handle, 0xA1, 0x2, 0);
+                }
+                catch
+                {
+                    return;
+                }
+            }
+        }
+
+        #endregion
+
+        public bool AeroActive
+        {
+            get
+            {
+                try
+                {
+                    if (Environment.OSVersion.Version.Major >= 6)
+                    {
+                        int enabled = 0;
+                        NativeMethods.DwmIsCompositionEnabled(ref enabled);
+                        return (enabled is 1);
+                    }
+                    else return false;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+        }
+
+        #region DWM Shadow
+
+        public void ShadowProc(ref Message m, IntPtr handle)
+        {
+            if ((uint)m.Msg is Constants.WM_NCPAINT)
+            {
+                if (AeroActive)
+                {
+                    var attrVal = 2;
+                    NativeMethods.DwmSetWindowAttribute(handle, 2, ref attrVal, 4);
+
+                    MARGINS wMargins = new MARGINS()
+                    {
+                        bottomHeight = 1,
+                        leftWidth = 1,
+                        rightWidth = 1,
+                        topHeight = 1
+                    };
+
+                    NativeMethods.DwmExtendFrameIntoClientArea(handle, ref wMargins);
+                }
+            }
+        }
+
+        public void ShadowProc(ref Message m, IntPtr handle, MARGINS margins)
+        {
+            if ((uint)m.Msg is Constants.WM_NCPAINT)
+            {
+                if (AeroActive)
+                {
+                    var attrVal = 2;
+                    NativeMethods.DwmSetWindowAttribute(handle, 2, ref attrVal, 4);
+                    NativeMethods.DwmExtendFrameIntoClientArea(handle, ref margins);
+                }
+            }
+        }
+
+        #endregion
+
+        #region Window Sizing
+
+        public const int RESIZE_HANDLE_SIZE = 5;
+
+
+        public void SizeProc(ref Message m, object source)
+        {
+            Type type = source.GetType().BaseType;
+
+            if (type.FullName != "System.Windows.Forms.Form")
+                return;
+
+            Form window = (Form)source;
+
+            if ((uint)m.Msg is Constants.WM_NCHITTEST)
+            {
+                if ((int)m.Result is 0x01)
+                {
+                    var screenPoint = new Point(m.LParam.ToInt32());
+                    var clientPoint = window.PointToClient(screenPoint);
+
+                    if (clientPoint.Y <= RESIZE_HANDLE_SIZE)
+                    {
+                        if (clientPoint.X <= RESIZE_HANDLE_SIZE)
+                            m.Result = (IntPtr)13;
+                        else if (clientPoint.X < (window.Size.Width - RESIZE_HANDLE_SIZE))
+                            m.Result = (IntPtr)12;
+                        else
+                            m.Result = (IntPtr)14;
+                    }
+                    else if (clientPoint.Y <= (window.Size.Height - RESIZE_HANDLE_SIZE))
+                    {
+                        if (clientPoint.X <= RESIZE_HANDLE_SIZE)
+                            m.Result = (IntPtr)10;
+                        else if (clientPoint.X < (window.Size.Width - RESIZE_HANDLE_SIZE))
+                            m.Result = (IntPtr)2;
+                        else
+                            m.Result = (IntPtr)11;
+                    }
+                    else
+                    {
+                        if (clientPoint.X <= RESIZE_HANDLE_SIZE)
+                            m.Result = (IntPtr)16;
+                        else if (clientPoint.X < (window.Size.Width - RESIZE_HANDLE_SIZE))
+                            m.Result = (IntPtr)15;
+                        else
+                            m.Result = (IntPtr)17;
+                    }
+                }
+            }
+        }
+
+        #endregion
+    }
+}
