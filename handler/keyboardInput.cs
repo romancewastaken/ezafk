48ae0f8a375943bf7596a209e6d5a2531e0c6488
romance <66000596+romance999@users.noreply.github.com>
2022-09-30 11:39:39 -0400

    Add files via upload

---
+++ handler/keyboardInput.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ezafk.handler
+{
+    internal class keyboardInput
+    {
+        public static int counter = 0;
+
+        public static string[] keyArray = new string[] { "W", "A", "S", "D" };
+
+        [DllImport("user32.dll")]
+        public static extern int SetForegroundWindow(IntPtr hWnd);
+        [STAThread]
+
+
+        public static void antiAfk()
+        {
+            try
+            {
+                Process[] processes = Process.GetProcessesByName(Data.valProcess);
+                while (Data.antiafk == true)
+                {
+
+                    foreach (Process proc in processes)
+                    {
+                        SetForegroundWindow(proc.MainWindowHandle);
+                        SendKeys.SendWait("A");
+                        Thread.Sleep(300);
+                        SendKeys.SendWait("A");
+                        Thread.Sleep(300);
+                        SendKeys.SendWait("D");
+                        Thread.Sleep(300);
+                        SendKeys.SendWait("D");
+                        Thread.Sleep(300);
+                        SendKeys.SendWait("W");
+                        Thread.Sleep(300);
+                        SendKeys.SendWait("S");
+                        Thread.Sleep(300);
+                        SendKeys.SendWait("S");
+                        Thread.Sleep(300);
+                        SendKeys.SendWait("W");
+                    }
+                    Thread.Sleep(300);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Exception caught when using anti afk: {ex.Message}", "ezafk");
+            }
+        }
+
+        public static void randomAntiAfk()
+        {
+            try
+            {
+                Process[] processes = Process.GetProcessesByName(Data.valProcess);
+                while (Data.antiafk == true)
+                {
+                    foreach (Process proc in processes)
+                    {
+                        Random random = new Random();
+                        int value = random.Next(0, keyArray.Length);
+                        SendKeys.SendWait(keyArray[value]);
+                    }
+                    Thread.Sleep(250); // make it slightly faster than the static afk function
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Exception caught when using random anti afk: {ex.Message}", "ezafk");
+            }
+        }
+    }
+}
